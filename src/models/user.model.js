3ae357d5523c9109d8a62a810f81457230711846
SyedAffan-o7 <syedaffan.o88z@gmail.com>
2025-02-13 11:37:43 +0530

    Added user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,86 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url where actual image will be stored in return it gives its url which we will store her
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url where actual image will be stored in return it gives its url which we will store her
+    },
+    watchHistory: [{ type: Schema.Types.ObjectId, ref: "Video" }],
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+// encripting password
+/*.pre = wiill do the changes in schema {userschema } befor the action in these case save it will encrypt password */
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+// designing custom methods
+// will check if password provided by user is correct bcrypt.compare method allows u to do that
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id, // since it refreshes evry time we need to store id of user
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+export const User = mongoose.model("User", userSchema);
+
+// jwt is bearer token which is used to authenticate user the one who has it sends data to them
